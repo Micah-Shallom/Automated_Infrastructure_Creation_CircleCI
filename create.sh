005417597bee3681c83df96e44438978c0846a3f
Micah Shallom <micahshallom@gmail.com>
2022-06-15 01:58:58 +0100

    new

--- create.sh
+++
@@ -1,1 +0,0 @@
-aws cloudformation create-stack --stack-name serverstack --template-body file://server.yml --parameters file://serverParams.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
